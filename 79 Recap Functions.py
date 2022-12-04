1c7b4fb3144bfa962889bb5965c7a9c8014ff20e
EmonArefin <shovon1218@gmail.com>
2022-12-04 20:01:14 +0600

    first time upload

---
+++ 79 Recap Functions.py
@@ -0,0 +1,14 @@
+def square(a):
+    """
+    Return square of a number
+    """
+    double = a * a
+    return double
+print(square(20))
+print(print.__doc__)
+print(square.__doc__)
+
+def multiplication(a,b):
+    multi = a * b
+    return multi
+print(multiplication(2, 3))
